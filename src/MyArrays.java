74674a1f1ebadeeeeccc9c58ff2c77505d921cb0
TarasovAlex <tarasof.av@gmail.com>
2022-11-14 22:38:24 +0200

    HW 5 at 14.11.2022

--- src/MyArrays.java
+++ src/MyArrays.java
@@ -23,7 +23,7 @@ public class MyArrays {
 
 	public static int[] removeNumber(int[] array, int index) {
 		int[] arrayres = array;
-		if (index < array.length) {
+		if (index > -1 && index < array.length) {
 			arrayres = new int[array.length - 1];
 			System.arraycopy(array, 0, arrayres, 0, index);
 			System.arraycopy(array, index + 1, arrayres, index, array.length - index - 1);
@@ -38,16 +38,105 @@ public class MyArrays {
 	 * @return new array with inserted number at index for keeping array sorted
 	 */
 	public static int[] insertSorted(int arraySorted[], int number) {
-		
+
 		int index = Arrays.binarySearch(arraySorted, number);
 		index = (index < 0) ? Math.abs(index) - 1 : index;
 		int[] arrayres = new int[arraySorted.length + 1];
-		
+
 		System.arraycopy(arraySorted, 0, arrayres, 0, index);
 		arrayres[index] = number;
 		System.arraycopy(arraySorted, index, arrayres, index + 1, arraySorted.length - index);
-		
+
 		return arrayres;
 	}
 
+	/**
+	 * 1. Update method binarySearch in the class MyArrays such that if a being
+	 * searched number is encountered several times in a given sorted array the
+	 * method should return the first index value, otherwise (no number in a given
+	 * array) the method should return the same negative value as the Java standard
+	 * Arrays.binarySearch does 1.1. Complexity of the method should be O[LogN]
+	 * 
+	 * @param arraySorted
+	 * @param number
+	 * @return index of first number or if number not exists in array return -1
+	 */
+
+	public static int binarySearch(int arraySorted[], int number) {
+		int left = 0;
+		int right = arraySorted.length - 1;
+		int res = 0;
+		while (left <= right) {
+			int middle = (left + right) / 2;
+			if (number == arraySorted[middle]) {
+				res = middle;
+				right = middle - 1;
+			} else if (number < arraySorted[middle]) {
+				right = middle - 1;
+			} else {
+				left = middle + 1;
+			}
+		}
+		return arraySorted[res] == number ? res : -1;
+	}
+
+	/**
+	 * 3.1. Bubble sort implies repeated moving greater values toward the end of
+	 * array until array is sorted. The moving is done by swapping between two
+	 * adjacent array elements (i with i+1). Consider method swap. 3.2. Apply
+	 * additional private method moveGreaterRight that in the cycle compares I-th
+	 * element with I+1-th element and if I-th value greater than I+1-th one the
+	 * swap is performed, thus it moves the greater value to right. 3.3.
+	 * Requirements: 3.3.1. Most effective algorithm in the class of algorithms O[N
+	 * ^ 2] 3.3.2. The minimal number of iterations (the array is already sorted)
+	 * should be N 3.3.3. The maximal number of iterations should be N * (N + 1) / 2
+	 * 
+	 * @param unSortArray
+	 * @return sortedArray
+	 */
+
+	static boolean FLAG = false;
+
+	public static int[] bubbleSort(int unSortedArray[]) {
+		while (!FLAG) {
+			FLAG = true;
+			for (int i = 0; i < unSortedArray.length - 1; i++)
+				moveGreaterRight(unSortedArray, i);
+		}
+		return unSortedArray;
+	}
+
+	private static void moveGreaterRight(int sortedArray[], int j) {
+		if (sortedArray[j] > sortedArray[j + 1]) {
+			int tempVar = sortedArray[j];
+			sortedArray[j] = sortedArray[j + 1];
+			sortedArray[j + 1] = tempVar;
+
+			FLAG = false;
+		}
+	}
+
+	/**
+	 * Write method isOneSwapForSorted in the class MyArrays that takes an array
+	 * and returns true if the array is unsorted but only one swap between two
+	 * numbers is required for getting a sorted array.
+	 * 
+	 * @param arrayUnSorted[]
+	 */
+
+	
+	public static int isOneSwapForSorted(int arrayUnSorted[]) {
+		int a = 0;
+		for (int i = 1; i < arrayUnSorted.length; i++) {
+			int current = arrayUnSorted[i];
+			int j = i;
+			while (j > 0 && arrayUnSorted[j - 1] > current) {
+				arrayUnSorted[j] = arrayUnSorted[j - 1];
+				j--;
+				a++;
+			}
+			arrayUnSorted[j] = current;
+		}
+		return a;
+	}
 }
--- src/PrimitivesTest.java
+++ src/PrimitivesTest.java
@@ -97,44 +97,108 @@ class PrimitivesTest {
 		assertEquals(false, Numbers.isHappyNumber(562123));
 		assertEquals(true, Numbers.isHappyNumber(100001));
 	}
+
 	@Test
 	@Disabled
 	void getDigitsTest() {
-		int expected[] = {1, 2, 3, 4};
+		int expected[] = { 1, 2, 3, 4 };
 		assertArrayEquals(expected, Numbers.getDigits(1234));
 	}
+
 	@Test
 	@Disabled
 	void getNumberFromDigitsTest() {
 		int expectedNumber = 1234;
-		assertEquals(expectedNumber, Numbers.getNumberFromDigits(new int[]{1, 2, 3, 4}));			
-		}
-	
+		assertEquals(expectedNumber, Numbers.getNumberFromDigits(new int[] { 1, 2, 3, 4 }));
+	}
+
 	@Test
+	@Disabled
 	void getAddsNumberTest() {
-		int arrayTest[] = {1, 3, 6, 5, 0};
+		int arrayTest[] = { 1, 3, 6, 5, 0 };
 		int number = 7;
 		System.out.println(Arrays.toString(MyArrays.addsNumber(arrayTest, number)));
 	}
-	
+
 	@Test
+	@Disabled
 	void getRemoveNumber() {
-		int arrTest[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-		int index = 5;
+		int arrTest[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		int index = 0;
 		System.out.println(Arrays.toString(arrTest));
 		System.out.println(Arrays.toString(MyArrays.removeNumber(arrTest, index)));
 	}
-	
+
 	@Test
+	@Disabled
 	void getInsertSorted() {
-		int arraySorted[] = {1, 2, 4, 5, 6, 7};
-		int number = 3;
+		int arraySorted[] = { 1, 2, 4, 5, 6, 7 };
+		int number = 1;
 		System.out.println(Arrays.toString(MyArrays.insertSorted(arraySorted, number)));
 	}
-	
+
 	@Test
+	@Disabled
 	void getIsraelIdentity() {
-		assertEquals(false, IsraelIdentity.verify(IsraelIdentity.generateRandomId(0, 9)));
-		assertEquals(true, IsraelIdentity.verify(IsraelIdentity.generateRandomId(0, 9)));
+		assertEquals(false, IsraelIdentity.verify(445862792));
+		assertEquals(true, IsraelIdentity.verify(445862790));
+	}
+
+	@Test
+	void getBinarySearch() {
+		int[] arrayTest = { 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 4, 4, 4, 5 };
+		assertEquals(3, MyArrays.binarySearch(arrayTest, 3));
+		assertEquals(12, MyArrays.binarySearch(arrayTest, 4));
+		assertEquals(15, MyArrays.binarySearch(arrayTest, 5));
+		assertEquals(0, MyArrays.binarySearch(arrayTest, 1));
+	}
+
+	@Test
+	void getBubbleSort() {
+		int[] bubbleSortTest1 = { 5, 3, 6, 8, 7, 6, 9, 15, 10, 20 };
+		int[] bubbleSortTrue1 = { 3, 5, 6, 6, 7, 8, 9, 10, 15, 20 };
+		int[] bubbleSortTest2 = {};
+		int[] bubbleSortTrue2 = {};
+		int[] bubbleSortTest3 = { 1, 2, 3, 4, 5 };
+		int[] bubbleSortTrue3 = { 1, 2, 3, 4, 5 };
+		assertArrayEquals(bubbleSortTrue1, MyArrays.bubbleSort(bubbleSortTest1));
+		assertArrayEquals(bubbleSortTrue2, MyArrays.bubbleSort(bubbleSortTest2));
+		assertArrayEquals(bubbleSortTrue3, MyArrays.bubbleSort(bubbleSortTest3));
+	}
+
+	@Test
+	void isOneSwapTestFalse() {
+		Int ar1[] = { 1, 2, 3, 10, -1, 5, 6 };
+		Int ar2[] = { 1, 2, 3, 4, 5, 10 };
+		Int ar3[] = { 5, 1, 2, 4, 6, 10 };
+		Int ar4[] = { 1, 5, 2, 4, 3, 10 };
+		Int ar5[] = { 1, 3, 2, 5, 4, 10, 8 };
+		Int ar6[] = { 1, 3, 20, 4, 5, 6, 10 };
+		Int ar7[] = { 1, 3, 20, 4, 5, 11, 2 };
+		assertFalse(MyArrays.isOneSwapForSorted(ar1));
+		assertFalse(MyArrays.isOneSwapForSorted(ar2));
+		assertFalse(MyArrays.isOneSwapForSorted(ar3));
+		assertFalse(MyArrays.isOneSwapForSorted(ar4));
+		assertFalse(MyArrays.isOneSwapForSorted(ar5));
+		assertFalse(MyArrays.isOneSwapForSorted(ar6));
+		assertFalse(MyArrays.isOneSwapForSorted(ar7));
+	}
+
+	@Test
+	void isOneSwapTestTrue() {
+		Int ar1[] = { 10, 2, 3, 4, 1 };
+		Int ar2[] = { 1, 2, 4, 3, 5, 10 };
+		Int ar3[] = { 1, 2, 3, 10, 5, 4 };
+		Int ar4[] = { 1, 5, 3, 4, 2, 10 };
+		Int ar5[] = { 1, 2, 3, 4, 10, 5 };
+		Int ar6[] = { 2, 1, -3, 4, 5, 10 };
+		Int ar7[] = { 3, 2, 1, 4, 5, 6 };
+		assertTrue(MyArrays.isOneSwapForSorted(ar1));
+		assertTrue(MyArrays.isOneSwapForSorted(ar2));
+		assertTrue(MyArrays.isOneSwapForSorted(ar3));
+		assertTrue(MyArrays.isOneSwapForSorted(ar4));
+		assertTrue(MyArrays.isOneSwapForSorted(ar5));
+		assertTrue(MyArrays.isOneSwapForSorted(ar6));
+		assertTrue(MyArrays.isOneSwapForSorted(ar7));
 	}
 }
